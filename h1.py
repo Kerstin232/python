5a56855be5a74d3819ab3c5b860f35d9079b7a6d
Kerstin232 <kerstindorrit@hkhk.edu.ee>
2024-12-05 14:50:48 +0200

    Update turtle

--- h1.py
+++ h1.py
@@ -5,7 +5,8 @@
 #see importib kilpkonna mooduli
 import turtle
 
-turtle.speed(0)
+#kolmnurk
+turtle.speed(2) #reguleeri 0-9
 turtle.penup()
 turtle.goto(-500,200)
 turtle.pendown()
@@ -16,6 +17,15 @@ turtle.left(120) # left right käsud -- lt/rt
 turtle.fd(200) #fd - forward lühi
 turtle.left(120) # left right käsud -- lt/rt
 
+#süda
+turtle.penup()
+turtle.goto(-200,200)
+turtle.pendown()
+turtle.fd(100)
+turtle.circle(50,180)
+turtle.right(90)
+turtle.circle(50,180)
+turtle.fd(100)
 
 #lõpetab kilpkonna kokku ei jookseks 
 turtle.done()
