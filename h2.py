0051adbbd844392f493b6c9d82d4f5dd9bcd88d8
Kerstin232 <kerstindorrit@hkhk.edu.ee>
2024-12-05 15:11:44 +0200

    h2

---
+++ h2.py
@@ -0,0 +1,44 @@
+#Ülesanne 2
+#05.12.2024
+import turtle
+#sinine kilpkonn
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-200,100)
+turtle.pendown()
+turtle.color("blue")
+turtle.pensize(6)
+turtle.circle(50,360)
+#must kilpkonn
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-90,100)
+turtle.pendown()
+turtle.color("black")
+turtle.pensize(6)
+turtle.circle(50,360)
+#punane
+turtle.speed(0)
+turtle.penup()
+turtle.goto(20,100)
+turtle.pendown()
+turtle.color("red")
+turtle.pensize(6)
+turtle.circle(50,360)
+#kollane
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-150,100)
+turtle.pendown()
+turtle.color("red")
+turtle.pensize(6)
+turtle.circle(50,360)
+#rohleline
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-30,100)
+turtle.pendown()
+turtle.color("green")
+turtle.pensize(6)
+turtle.circle(50,360)
+
