64348849a9760a8e69da913c3e6b94c0a94ac62d
Kerstin232 <kerstindorrit@hkhk.edu.ee>
2024-12-05 14:37:56 +0200

    kilpkonn

---
+++ h1.py
@@ -0,0 +1,21 @@
+# 01 ülesanne
+#kerstin
+
+
+#see importib kilpkonna mooduli
+import turtle
+
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-500,200)
+turtle.pendown()
+turtle.fd(200) #fd - forward lühi
+turtle.left(120) # left right käsud -- lt/rt
+turtle.fd(200) #fd - forward lühi
+turtle.left(120) # left right käsud -- lt/rt
+turtle.fd(200) #fd - forward lühi
+turtle.left(120) # left right käsud -- lt/rt
+
+
+#lõpetab kilpkonna kokku ei jookseks 
+turtle.done()
